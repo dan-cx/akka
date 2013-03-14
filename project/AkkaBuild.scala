a4447d1e91984679ec7d5afa111068f74a0ce697
Daniel McDonald <daniel@cx.com>
2013-03-14 13:09:41 -0700

    Update protobuf to 2.5.0
    See #3154 for details.

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -862,7 +862,7 @@ object Dependencies {
 
     val config        = "com.typesafe"                % "config"                       % "1.0.0"       // ApacheV2
     val netty         = "io.netty"                    % "netty"                        % "3.6.1.Final" // ApacheV2
-    val protobuf      = "com.google.protobuf"         % "protobuf-java"                % "2.4.1"       // New BSD
+    val protobuf      = "com.google.protobuf"         % "protobuf-java"                % "2.5.0See"       // New BSD
     val scalaStm      = "org.scala-stm"              %% "scala-stm"                    % "0.7"         // Modified BSD (Scala)
 
     val slf4jApi      = "org.slf4j"                   % "slf4j-api"                    % "1.7.2"       // MIT
